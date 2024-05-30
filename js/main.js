08f09881c5a3a4a03954d3130e75106390c10dbe
kevin <kevin.rojokevin17@gmail.com>
2024-05-30 19:23:12 -0300

    Codigo mejorado

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 import { cancion1, cancion2, cancion3 } from "./canciones.js";
 
 let cambio = document.querySelector(".cambiar");
+let reproductor = document.querySelector(".play-1");
 let musicas = [cancion1, cancion2, cancion3];
-
 let posicion = 0;
+let song = new Audio();
+let play, pausa, siguiente, anterior, range;
 
 function mostrarCanciones(obj) {
-  cambio.innerHTML = `<img src=${obj.imagen} alt="imagen" class="img" />
-  <p class="text-1">${obj.nombre}</p>
-  <p class="text-2">${obj.cantante}</p>
-   
-`;
+  cambio.innerHTML = "";
+  const foto = document.createElement("img");
+  foto.src = obj.imagen;
+  foto.alt = "imagen";
+  foto.classList.add("img");
+  cambio.appendChild(foto);
+  const text1 = document.createElement("p");
+  text1.textContent = obj.nombre;
+  text1.classList.add("text-1");
+  cambio.appendChild(text1);
+  const text2 = document.createElement("p");
+  text2.textContent = obj.cantante;
+  text2.classList.add("text-2");
+  cambio.appendChild(text2);
   const div = document.createElement("div");
   div.id = "duracion";
   cambio.appendChild(div);
@@ -20,8 +31,9 @@ function mostrarCanciones(obj) {
   const final = document.createElement("p");
   final.textContent = "0";
   div.appendChild(final);
-  let song = new Audio(obj.cancion);
+
   song.src = obj.cancion;
+  song.load();
   song.autoplay = true;
   cambio.appendChild(song);
   const range = document.createElement("input");
@@ -32,14 +44,6 @@ function mostrarCanciones(obj) {
   range.value = 0;
   range.id = "range";
   cambio.appendChild(range);
-
-  function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    return `${minutes.toString().padStart(2, "0")}:${remainingSeconds
-      .toString()
-      .padStart(2, "0")}`;
-  }
 
   song.addEventListener("loadedmetadata", () => {
     range.max = song.duration;
@@ -55,58 +59,67 @@ function mostrarCanciones(obj) {
     song.currentTime = range.value;
   });
 
-  const play = document.querySelector(".play");
+  actualizarControles();
+}
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  return `${minutes.toString().padStart(2, "0")}:${remainingSeconds
+    .toString()
+    .padStart(2, "0")}`;
+}
 
-  play.addEventListener("click", () => {
+function actualizarControles() {
+  play = document.querySelector(".play");
+  anterior = document.querySelector(".izquierda");
+  pausa = document.querySelector(".pause");
+  siguiente = document.querySelector(".derecha");
+
+  play.removeEventListener("click", handelPlay);
+  pausa.removeEventListener("click", playOff);
+  siguiente.removeEventListener("click", sig);
+  anterior.removeEventListener("click", ant);
+
+  play.addEventListener("click", handelPlay);
+  pausa.addEventListener("click", playOff);
+  siguiente.addEventListener("click", sig);
+  anterior.addEventListener("click", ant);
+
+  siguiente.disabled = posicion === musicas.length - 1;
+  anterior.disabled = posicion === 0;
+
+  function handelPlay() {
     song.play();
     play.classList.toggle("pause");
     pausa.classList.toggle("pause");
-  });
+  }
 
-  const pausa = document.querySelector(".pause");
-  pausa.addEventListener("click", () => {
+  function playOff() {
     song.pause();
     play.classList.toggle("pause");
     pausa.classList.toggle("pause");
-  });
-  const siguiente = document.querySelector(".derecha");
-  siguiente.addEventListener("click", () => {
-    if (posicion < musicas.length) {
+  }
+
+  function sig() {
+    if (posicion < musicas.length - 1) {
       posicion++;
       mostrarCanciones(musicas[posicion]);
-      console.log(musicas[posicion]);
     } else {
       siguiente.disableb = true;
       console.log("no hay mas musica");
     }
-    musicas[posicion - 1] = disableb;
-  });
-  document.querySelector(".izquierda").addEventListener("click", () => {
+  }
+  function ant() {
     if (posicion > 0) {
       posicion--;
       mostrarCanciones(musicas[posicion]);
     } else {
+      siguiente.disableb = true;
       console.log("no hay mas musica");
     }
-  });
-
-  elementos();
-}
-
-function elementos() {
-  song.src = musicas[posicion].cancion;
-  siguiente.disabled = posicion === musicas.length - 1;
-  anterior.disabled = posicion === 0;
-}
-
-mostrarCanciones(musicas[posicion]);
-
-function fin() {
-  if (range.value == song.duration) {
-    posicion++;
-    mostrarCanciones(musicas[posicion]);
   }
 }
+mostrarCanciones(musicas[posicion]);
 
 /*
 function elementos() {
@@ -116,35 +129,4 @@ function elementos() {
 }
 
 
-
-
-
-siguiente.addEventListener("click", () => {
-  if (posicion < musicas.length - 1) {
-    posicion++;
-    elementos();
-  } else {
-    siguiente.disableb = true;
-    console.log("no hay mas musica");
-  }
-  siguiente.disableb = false;
-});
-
-function elementos() {
-  song.src = musicas[posicion];
-  siguiente.disableb = posicion === musicas.length - 1;
-  anterior.disableb = posicion === 0;
-}
-
-
-
-anterior.addEventListener("click", () => {
-  if (posicion > 0) {
-    posicion--;
-    elementos();
-  } else {
-    anterior.disableb = true;
-    console.log("no hay mas musica");
-  }
-  siguiente.disableb = false;
-});*/
+*/
